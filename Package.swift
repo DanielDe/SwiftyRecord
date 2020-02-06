0912a32beb84c34faa4d7673cfbc23963bfc37b1
Daniel de Haas <daniel.andrew.dehaas@gmail.com>
2020-02-06 11:07:37 -0800

    add platforms and dependencies

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
   name: "SwiftyRecord",
+  platforms: [.macOS(.v10_15), .iOS(.v13)],
   products: [
     // Products define the executables and libraries produced by a package, and make them visible to other packages.
     .library(
@@ -21,7 +22,7 @@ let package = Package(
     // Targets can depend on other targets in this package, and on products in packages which this package depends on.
     .target(
       name: "SwiftyRecord",
-      dependencies: []),
+      dependencies: ["SQLite", "Runtime"]),
     .testTarget(
       name: "SwiftyRecordTests",
       dependencies: ["SwiftyRecord"]),
